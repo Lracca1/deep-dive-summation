b9bb4dcb1d12fbb86151c4b2b358742f0d5a714d
lracca1 <lracca@cnm.edu>
2018-05-23 14:53:03 -0600

    Added more operations and empty stack message.

--- src/edu/cnm/deepdive/RpnCalculator.java
+++ src/edu/cnm/deepdive/RpnCalculator.java
@@ -17,8 +17,7 @@ public class RpnCalculator {
    */
   public static void main(String[] args) {
     Stack<Double> stack = new Stack<>();
-    for (int i = 0; i < args.length; i++) {
-      String arg = args[i];
+    for (String arg : args) {
      arg =arg.trim().toLowerCase(); 
       switch (arg) {
         case "add":
@@ -41,14 +40,32 @@ public class RpnCalculator {
         case "^":
           power(stack);
           break;
+        case "abs":
+          abs(stack);
+          break;
+        case "log":
+          log10(stack);
+          break;
+        case "pop":
+          stack.pop();
+          break;
+        case "idiv":
+        case "\\":
+          idiv(stack);
+          break;
+        case "swap":
+          swap(stack);
+          break;
         default:
           stack.push(Double.valueOf(arg));
           break;
       }
-      
-      
     }
-    System.out.printf("Result = %,.2f",stack.pop());
+    if (stack.empty()) {
+      System.out.println("Stack is empty.");
+    } else {
+      System.out.printf("Result = %,.2f", stack.pop());
+    }
   }
 
   /**
@@ -79,6 +96,29 @@ public class RpnCalculator {
     stack.push(Math.pow(values[0], values[1]));
   }
 
+  private static void abs(Stack<Double> stack) {
+    double value = (stack.pop());
+    stack.push(Math.abs(value));
+  }
+
+  private static void log10(Stack<Double> stack) {
+    double value = (stack.pop());
+    stack.push(Math.log10(value));
+  }
+
+  private static void idiv(Stack<Double> stack) {
+    double[] values = pop(stack, 2);
+    double result = values[0] / values[1];
+    double quotient = Math.floor(result);
+    double remainder = values[0] - values[1] * quotient;
+    stack.push(remainder);
+    stack.push(quotient);
+  }
+  private static void swap(Stack<Double> stack) {
+    double[] values = pop(stack, 2);
+    stack.push(values[0]);
+    stack.push(values[1]);
+  }
 
   private static double[] pop(Stack<Double> stack, int numItems) {
     double[] values = new double[numItems];
